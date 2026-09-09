c064dd0046972683806d20325283372e02e997e6
Pallavi Jadhav <134396674+pallujadhav19@users.noreply.github.com>
2026-09-09 13:04:22 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.java
@@ -0,0 +1,55 @@
+
+class DynamicArray {
+
+    private int[] arr;
+    private int size;
+    private int capacity;
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.arr = new int[capacity];
+    }
+
+    public int get(int i) {
+        return arr[i];
+    }
+
+    public void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (size == capacity) {
+            resize();
+        }
+
+        arr[size] = n;
+        size++;
+    }
+
+    public int popback() {
+        size--;
+        return arr[size];
+    }
+
+    public void resize() {
+        int[] newArr = new int[capacity * 2];
+
+        for (int i = 0; i < size; i++) {
+            newArr[i] = arr[i];
+        }
+
+        arr = newArr;
+        capacity *= 2;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
+
